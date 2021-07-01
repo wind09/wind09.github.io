2344e0b4c017f8e4638bfc9086ec67199a95abcf
wind09 <chenkan0007@gmail.com>
2021-07-01 01:21:38 -0700

    update contact

--- js/contact.js
+++ js/contact.js
@@ -8,7 +8,7 @@ function sendMail() {
 	var from = $("textarea#msg_from_email").val();
 	var subject = $("textarea#msg_subject").val();
 	var content = $("textarea#msg_content").val();
-	var link = 'mailto:daizihang@cmu.edu?cc=' + from
+	var link = 'mailto:kanchen@waymo.com?cc=' + from
 				+'&subject='+subject+'&body='+content;
 	window.location.href = link;
 }
